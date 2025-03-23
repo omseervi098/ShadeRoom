e06c85a7b233b56cff23882c2c0f8ff6cfdcec3d
Omprakash Choudhary <omprakash.work96@gmail.com>
2025-03-23 22:43:10 +0530

    added missing dependency for netlify deploy

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig(({mode})=>({
-    plugins: [
-        react(),
-        tailwindcss()
-    ],
-    esbuild: {
-        drop: mode === 'production' ? ['console', 'debugger'] : [], // Removes console logs in production
-    },
-}))
+export default defineConfig(({ mode }) => ({
+  plugins: [react(), tailwindcss()],
+  esbuild: {
+    drop: mode === "production" ? ["console", "debugger"] : [], // Removes console logs in production
+  },
+}));
